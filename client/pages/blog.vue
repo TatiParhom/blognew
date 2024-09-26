06eba27e1b71baebf920093e0ef4815b1d043d86
Tatiparhom <tvparhom@bk.ru>
2024-09-26 21:11:28 +0300

    новый

---
+++ client/pages/blog.vue
@@ -0,0 +1,34 @@
+<template>
+    <h2>Блог</h2>
+
+    <main>
+        <article v-for="post in posts" :key="post.id">
+            <h3>{{ post.title }}</h3>
+            <img :src=base_url+post.img.url :alt=post.img.alternativeText>
+            <p>{{ post.desc }}</p>
+            <NuxtLink to="#">Подробнее</NuxtLink>
+        </article>
+    </main>
+
+</template> 
+
+<script setup>
+    const api = await $fetch('http://localhost:1337/api/posts?populate=*')
+    const posts = api.data
+
+    const base_url = "http://localhost:1337"
+</script>
+
+<style scoped>
+    main {
+        display: flex;
+        gap: 10px;
+    }
+    article {
+        background-color: aliceblue;
+    }
+
+    img {
+        width: 300px;
+    }
+</style>
---
+++ client/pages/contact.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>Контакы</h2>
+</template>
